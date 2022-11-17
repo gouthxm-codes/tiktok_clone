b8c3dde1ef189f668cdd17ab562c9a268e17266e
gouthxm-codes <smithark01@gmail.com>
2022-11-17 17:44:42 +0530

    ...

--- store/authStore.ts
+++ store/authStore.ts
@@ -2,7 +2,7 @@ import create from 'zustand'
 import { persist } from 'zustand/middleware'
 import axios from 'axios'
 
-const url = 'http://localhost:3000'
+const url = process.env.BASE_URL
 
 const authStore = (set: any) => ({
     userProfile: null,
--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import jwt_decode from 'jwt-decode'
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
+const URL = process.env.BASE_URL
 export const createOrGetUser = async (response: any, addUser: any) => {
   const decoded: { name: string, picture: string, sub: string } = jwt_decode(response.credential)
 
@@ -18,6 +18,6 @@ export const createOrGetUser = async (response: any, addUser: any) => {
 
   addUser(user)
 
-  await axios.post(`http://localhost:3000/api/auth`, user)
+  await axios.post(`${URL}/api/auth`, user)
 
 }
